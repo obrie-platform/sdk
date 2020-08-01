a751cad7f66d2fb433e64bf63f607425f2089d26
eugenepisotsky <revanscript@gmail.com>
2020-08-01 23:37:47 +0300

    fix(core): fix

--- packages/react-components/src/widgets/SizedBox.tsx
+++ packages/react-components/src/widgets/SizedBox.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import {SizedBoxProps} from "@obrie/sdk";
 
 export interface ReactSizedBoxProps extends SizedBoxProps {
+    children?: React.ReactNode
 }
 
 const sizedBoxView = (props: any): any => null;
